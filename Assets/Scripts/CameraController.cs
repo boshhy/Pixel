7e9b67a8f25cbdbf658d1b4675b6d842d0dca8ee
boshhy <boshhy101@yahoo.com>
2023-05-01 16:38:22 -0700

    clamped x and y values for camera, need to clamp player x position

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float minHeight, maxHeight;
+    public float minHorizontal, maxHorizontal;
+    private int offset = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        // float clampedY = Mathf.Clamp(transform.position.y, minHeight, maxHeight);
+
+        // transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
+
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, minHorizontal, maxHorizontal),
+                                         Mathf.Clamp(target.position.y, minHeight, maxHeight) + offset, 
+                                         transform.position.z);
+    }
+}
