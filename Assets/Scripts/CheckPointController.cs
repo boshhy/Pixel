868fad85b058de380097c7026cfec4ba9c4d6f74
boshhy <boshhy101@yahoo.com>
2023-05-02 13:23:44 -0700

    Added checkpoint flags (does not do anything at the moment)

---
+++ Assets/Scripts/CheckPointController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour
+{
+    public static CheckPointController instance;
+
+    private checkPoint[] checkpoints;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkpoints = FindObjectsOfType<checkPoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DeactivateCheckPoints()
+    {
+        for (int i = 0; i < checkpoints.Length; i++)
+        {
+            checkpoints[i].ResetCheckPoint();
+        }
+    }
+}
---
+++ Assets/Scripts/checkPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPoint : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            CheckPointController.instance.DeactivateCheckPoints();
+
+            animator.SetBool("checkPointOn", true);
+        }
+    }
+
+    public void ResetCheckPoint()
+    {
+        animator.SetBool("checkPointOn", false);
+    }
+}
