64765f5b5b97ad05a7e693c12b1ea90afe984553
boshhy <boshhy101@yahoo.com>
2023-06-29 15:59:29 -0700

    Enemy pig now moves back and forth from pointA and pointB

---
+++ Assets/Scripts/PigController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigController : MonoBehaviour
+{
+    public float pigSpeed;
+    public Transform pointA;
+    public Transform pointB;
+
+    public bool movingLeft;
+
+    private Rigidbody2D rBody;
+    private SpriteRenderer sRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        sRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(movingLeft)
+        {
+            sRenderer.flipX = false;
+            rBody.velocity = new Vector2(-pigSpeed, rBody.velocity.y);
+            if(transform.position.x < pointA.position.x)
+            {
+                movingLeft = false;
+            }
+        }
+        else
+        {
+            sRenderer.flipX = true;
+            rBody.velocity = new Vector2(pigSpeed, rBody.velocity.y);
+            if(transform.position.x > pointB.position.x)
+            {
+                movingLeft = true;
+            }
+        }
+        
+    }
+}
