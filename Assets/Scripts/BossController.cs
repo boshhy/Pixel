486b45c07b9577a17d80f057ffae246bec588c77
boshhy <boshhy101@yahoo.com>
2023-07-10 16:41:27 -0700

    Trunk now attacks and shoots bullets

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -1,9 +1,37 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
+//     public float trunkSpeed;
+//     public float runningRange;
+//     private Transform player;
+//     private Rigidbody2D rBody;
+
+//     private void Start() 
+//     {
+//         rBody = GetComponent<Rigidbody2D>();
+//         player = GameObject.FindGameObjectWithTag("Player").transform;
+//     }
+
+//     private void Update() 
+//     {
+//         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+//         if (distanceFromPlayer < runningRange)
+//         {
+//             rBody.velocity = new Vector2(-trunkSpeed, rBody.velocity.y);
+//             // transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime); 
+//         }
+//     }
+
+//     void OnDrawGizmosSelected()
+//     {
+//         Gizmos.color = Color.green;
+//         Gizmos.DrawWireSphere(transform.position, runningRange);
+//     }
+// }
+
     public int trunkHealth;
     public float trunkSpeed;
     public Transform pointA;
@@ -16,36 +44,56 @@ public class BossController : MonoBehaviour
     private float currentWaitTime;
 
     public bool movingLeft;
+    public float runningRange;
+    public float firingRange;
 
+    // public float originalTimeForFiringBullet;
+    // private float timeForFiringBullet;
+    public GameObject bullet;
+    public Transform firePoint;
+    public Transform firePointB;
+
+    private Transform player;
     private Rigidbody2D rBody;
     private SpriteRenderer sRenderer;
     private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-                rBody = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rBody = GetComponent<Rigidbody2D>();
         sRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         currentMoveTime = maxMoveTime;
+        // timeForFiringBullet = originalTimeForFiringBullet;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentMoveTime > 0)
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        //Debug.Log("Trunk position: " + transform.position.x + " point A position: " + pointA.position.x + " point B position:" + pointB.position.x);
+        if (distanceFromPlayer < runningRange)
         {
-            currentMoveTime = currentMoveTime - Time.deltaTime;
-            if (movingLeft)
+            // RUN
+            //anim.SetBool("isJumping", false);
+            anim.SetBool("isFiring", false);
+            anim.SetBool("isRunning", true);
+
+            if (movingLeft) 
             {
+                //Debug.Log("should be running left");
                 sRenderer.flipX = false;
                 rBody.velocity = new Vector2(-trunkSpeed, rBody.velocity.y);
                 if(transform.position.x < pointA.position.x)
                 {
+                    Debug.Log("movingLeft = false");
                     movingLeft = false;
                 }
             }
             else
             {
+                //Debug.Log("should be running right");
                 sRenderer.flipX = true;
                 rBody.velocity = new Vector2(trunkSpeed, rBody.velocity.y);
                 if(transform.position.x > pointB.position.x)
@@ -53,27 +101,80 @@ public class BossController : MonoBehaviour
                     movingLeft = true;
                 }
             }
-            if (currentMoveTime <= 0)
-            {
-                currentWaitTime = Random.Range(maxWaitTime * 0.5f, maxWaitTime);
-                anim.SetBool("isRunning", false);
-            }
+
         }
-        else if (currentWaitTime > 0)
+        else if (distanceFromPlayer < firingRange)
         {
-            currentWaitTime = currentWaitTime - Time.deltaTime;
-            rBody.velocity = new Vector2(0.0f, rBody.velocity.y);
+            // Fire Bullet
+            //anim.SetBool("isJumping", false);
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isFiring", true);
+            //timeForFiringBullet -= Time.deltaTime;
+            // if (timeForFiringBullet <= 0.0f)
+            // {
+            //     anim.SetBool("isFiring", false);
+            //     anim.SetBool("isJumping", true);
+            //     timeForFiringBullet = originalTimeForFiringBullet;
+            // }
+            // else {
+            //     anim.SetBool("isJumping", false);
+            //     anim.SetBool("isFiring", true);
+            // }
 
-            if (currentWaitTime <= 0)
+            if (player.position.x >= transform.position.x)
             {
-                currentMoveTime = Random.Range(maxMoveTime * 0.5f, maxMoveTime);
-                anim.SetBool("isRunning", true);
+                movingLeft = false;
+                sRenderer.flipX = true;
             }
-
-
+            else
+            {
+                movingLeft = true;
+                sRenderer.flipX = false;
+            }
+            rBody.velocity = new Vector2(0.0f, rBody.velocity.y);
+        }
+        else
+        {
+            // Idle
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isFiring", false);
+            //anim.SetBool("isJumping", false);
+            if (player.position.x >= transform.position.x)
+            {
+                movingLeft = false;
+                sRenderer.flipX = true;
+            }
+            else
+            {
+                movingLeft = true;
+                sRenderer.flipX = false;
+            }
+            rBody.velocity = new Vector2(0.0f, rBody.velocity.y);
         }
         
     }        
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, runningRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, firingRange);
+    }
+
+    private void fireBullet()
+    {
+        if (movingLeft)
+        {
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+        }
+        else 
+        {
+            var newBullet = Instantiate(bullet, firePointB.position, firePoint.rotation);
+            newBullet.GetComponent<SpriteRenderer>().flipX = true;
+            newBullet.GetComponent<bullet>().speed = -newBullet.GetComponent<bullet>().speed;
+        }
+        
+    }
 }
 
 
