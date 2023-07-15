9f8ab4623badf05c946073fbbc99ba489037e922
boshhy <boshhy101@yahoo.com>
2023-07-14 17:34:57 -0700

    Added empty starting, playing, and ending scenes. Made key and door

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    public GameObject theClosedDoor;
+    public GameObject collectEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            theClosedDoor.GetComponent<DoorController>().PlayerHasKey = true;
+            Destroy(gameObject);
+            Instantiate(collectEffect, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+}
